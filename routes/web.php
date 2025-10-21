bde317c6aacdbb11e09dbf0e7f130fe2872fe027
surxam <suriam.agesilas@gmail.com>
2025-10-21 17:25:41 -0400

    modif route layouts controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BoutiqueController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [BoutiqueController::class, 'index'])->name('boutique.index');
